70ae9c5f8efd2aadaf9b253683734fe4f74302e4
Delia Fuhrmann <delia.fuhrmann.13@gmail.com>
2017-12-08 11:18:11 +0000

    Update Function_Extract_Model_Fit_Stats_Lavaan.R

--- Function_Extract_Model_Fit_Stats_Lavaan.R
+++ Function_Extract_Model_Fit_Stats_Lavaan.R
@@ -27,7 +27,7 @@ model.fit.stats <- function(x) {
   # format values
   table$chisq.scaled = sprintf("%.2f", round(table$chisq.scaled, 2)) # round 2 to digits
   table$pvalue.scaled = rd(table$pvalue.scaled, digits = 3) # round 3 to digits, remove leading 0
-  table$pvalue.scaled[grepl(".000", table$pvalue.scaled, ignore.case=F)] <- ".001" # if smaller than .001 write "< .001"
+  table$pvalue.scaled[grepl(".000", table$pvalue.scaled, ignore.case=F)] <- ".001" # if smaller than .001 write ".001"
   table$rmsea.ci.lower.scaled = rd(table$rmsea.ci.lower.scaled, digits = 3) # round 3 to digits, remove leading 0
   table$rmsea.ci.upper.scaled = rd(table$rmsea.ci.upper.scaled, digits = 3) # round 3 to digits, remove leading 0
   table$rmsea.scaled = rd(table$rmsea.scaled, digits = 3) # round 3 to digits, remove leading 0
@@ -35,11 +35,11 @@ model.fit.stats <- function(x) {
   table$srmr = rd(table$srmr, digits = 3) # round 3 to digits, remove leading 0
   
   # coerce into character
-  out = paste(c("X2(", table$df.scaled, ") = ", table$chisq.scaled,
-                ", p ", sign, " ", table$pvalue.scaled, "; RMSEA = ",
-                table$rmsea.scaled, " [", table$rmsea.ci.lower.scaled, " ",
-                table$rmsea.ci.upper.scaled, "]; CFI = ", table$cfi.scaled,
-                " ; SRMR = ", table$srmr),
+  out = paste(c("X2(", table$df.scaled, ")=", table$chisq.scaled,
+                ",p", sign, table$pvalue.scaled, "; RMSEA=",
+                table$rmsea.scaled, " [", table$rmsea.ci.lower.scaled,
+                table$rmsea.ci.upper.scaled, "]; CFI=", table$cfi.scaled,
+                "; SRMR=", table$srmr),
               collapse = "")
   
   return(out)
